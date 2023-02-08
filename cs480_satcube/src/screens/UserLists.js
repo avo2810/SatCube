7eacfee4c17eee5138e237ac77c8661c1b2a48f5
Thy Anh <thyanh@Thys-MacBook-Pro.local>
2023-02-08 11:31:14 -0800

    add function to delete user

--- cs480_satcube/src/screens/UserLists.js
+++ cs480_satcube/src/screens/UserLists.js
@@ -1,9 +1,15 @@
 import React, { Component, useEffect, useState } from "react";
 import "./UserLists.css";
+import {faTrash} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const UsersList = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
+    getAllUsers()
+  }, []);
+
+  const getAllUsers = () =>{
     fetch("http://localhost:4000/getAllUsers", {
       method: "GET",
     })
@@ -11,7 +17,31 @@ const UsersList = () => {
       .then((data) => {
         setData(data.data);
       });
-  }, []);
+  }
+
+  const deleteUser =(id, firstName, lastName) =>{
+    if(window.confirm(`Are you sure you want to delete ${firstName} ${lastName}`)) {
+      fetch("http://localhost:4000/deleteUser", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        userID: id
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert(data.data)
+        getAllUsers()
+      })
+    } else {
+
+    }
+  }
   return (
     <div className="auth-wrapper">
       <div className="auth-inner" style={{ width: "auto" }}>
@@ -32,7 +62,9 @@ const UsersList = () => {
                   </td>
                   <td>{i.email}</td>
                   <td>{i.userType} </td>
-                  <td></td>
+                  <td>
+                    <FontAwesomeIcon icon={faTrash} onClick={()=>deleteUser(i._id, i.firstName, i.lastName)}/>
+                  </td>
                 </tr>
               );
             })}
