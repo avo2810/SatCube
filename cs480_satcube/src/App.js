625bc86a65d1a04da70e3d3c071a2559ec60c561
Thy Anh <thyanh@Thys-MacBook-Pro.local>
2023-02-09 21:14:12 -0800

    design dashboard page

--- cs480_satcube/src/App.js
+++ cs480_satcube/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import Signin from "./screens/Signin";
 import SignUp from "./screens/Signup";
@@ -9,6 +9,7 @@ import Dashboard from "./screens/Dashboard";
 import UserInfo from "./screens/UserInfo";
 import ResetPassword from "./screens/ResetPassword";
 import UserLists from "./screens/UserLists";
+import DashboardRoot from "./screens/DashboardRoot";
 
 function App() {
   const isLoggedIn = window.localStorage.getItem("loggedIn");
@@ -21,14 +22,18 @@ function App() {
             path="/"
             // This one is to verrify if the user is successfully logged in,
             // if so, we can just show the dashboard, if not, show the sign-in page
-            element={isLoggedIn ? <Dashboard /> : <Signin />}
-          />
+            element={isLoggedIn ? <DashboardRoot /> : <Signin />}
+          >
+          
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/userInfo" element={<UserInfo />} />
+            <Route path="/user-lists" element={<UserLists />} />
+          </Route>
           <Route path="/sign-in" element={<Signin />} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/userInfo" element={<UserInfo />} />
+          
           <Route path="/forgot-password" element={<ResetPassword />} />
-          <Route path="/user-lists" element={<UserLists />} />
+          
         </Routes>
       </div>
     </Router>
---
+++ cs480_satcube/src/screens/DashboardRoot.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+import DashBoardNavigation from "../components/DashboardNavigation"
+
+const DashboardRoot = () => {
+    return(
+        <div>
+            <DashBoardNavigation/>
+            <Outlet/>
+        </div>
+    )
+}
+
+export default DashboardRoot
