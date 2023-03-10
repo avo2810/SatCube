de1a722aa70b728fae3f996239c1c580a14d0daa
Thy Anh <thyanh@Thys-MBP.lan>
2023-03-09 19:42:44 -0800

    add stripe payment

--- cs480_satcube/src/screens/DashboardRoot.js
+++ cs480_satcube/src/screens/DashboardRoot.js
@@ -1,13 +1,13 @@
-import { Outlet } from "react-router-dom"
-import DashBoardNavigation from "../components/DashboardNavigation"
+import { Outlet } from "react-router-dom";
+import DashBoardNavigation from "../components/DashboardNavigation";
 
 const DashboardRoot = () => {
-    return(
-        <div>
-            <DashBoardNavigation/>
-            <Outlet/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <DashBoardNavigation />
+      <Outlet />
+    </div>
+  );
+};
 
-export default DashboardRoot
+export default DashboardRoot;
--- cs480_satcube/src/screens/UserInfo.js
+++ cs480_satcube/src/screens/UserInfo.js
@@ -1,14 +1,52 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./UserInfo.css";
+import { loadStripe } from "@stripe/stripe-js";
 
-export default class UserInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userData: "",
-    };
+let stripePromise;
+let key = process.env.REACT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
+console.log(key);
+const getStripe = () => {
+  if (!stripePromise) {
+    //input  stripe key here
+    stripePromise = loadStripe("");
   }
-  componentDidMount() {
+
+  return stripePromise;
+};
+
+const UserInfo = () => {
+  const [userData, setUserData] = useState("");
+  const [stripeError, setStripeError] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  const item = {
+    //input price key here
+    price: "",
+    quantity: 1,
+  };
+
+  const checkoutOptions = {
+    lineItems: [item],
+    mode: "subscription",
+    successUrl: `${window.location.origin}/success`,
+    cancelUrl: `${window.location.origin}/cancel`,
+  };
+
+  const redirectToCheckout = async () => {
+    setLoading(true);
+    console.log("redirectToCheckout");
+
+    const stripe = await getStripe();
+    const { error } = await stripe.redirectToCheckout(checkoutOptions);
+    console.log("Stripe checkout error", error);
+
+    if (error) setStripeError(error.message);
+    setLoading(false);
+  };
+
+  if (stripeError) alert(stripeError);
+
+  useEffect(() => {
     fetch("http://localhost:4000/userData", {
       method: "POST",
       crossDomain: true,
@@ -24,19 +62,28 @@ export default class UserInfo extends Component {
       .then((res) => res.json())
       .then((data) => {
         console.log(data, "userData");
-        this.setState({ userData: data.data });
+
+        setUserData(data.data);
       });
-  }
-  render() {
-    return (
-      <div className="auth-wrapper">
-        <div className="auth-inner" style={{ width: "auto" }}>
-          <h1>Account Information</h1>
-          <h3>First Name: {this.state.userData.firstName}</h3>
-          <h3>Last Name: {this.state.userData.lastName}</h3>
-          <h3>UserType: {this.state.userData.userType}</h3>
-        </div>
+  }, []);
+
+  return (
+    <div className="auth-wrapper">
+      <div className="auth-inner" style={{ width: "auto" }}>
+        <h1>Account Information</h1>
+        <h3>First Name: {userData.firstName}</h3>
+        <h3>Last Name: {userData.lastName}</h3>
+        <h3>UserType: {userData.userType}</h3>
+        <button
+          className="checkout-button"
+          onClick={redirectToCheckout}
+          disabled={isLoading}
+        >
+          <p className="text">{isLoading ? "Loading..." : "Subscribe"}</p>
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default UserInfo;
