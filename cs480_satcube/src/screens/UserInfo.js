53845ac5c6e430560cdee247150f0e7e7bd9931d
Thy Anh <thyanh@Thys-MacBook-Pro.local>
2023-04-17 21:36:26 -0700

    finish edit user info outline

--- cs480_satcube/src/screens/UserInfo.js
+++ cs480_satcube/src/screens/UserInfo.js
@@ -7,6 +7,7 @@ const UserInfo = () => {
   const [regularUser, setRegularUser] = useState(false);
   const [subscribedUser, setSubscribedUser] = useState(false);
   const [admin, setAdmin] = useState(false);
+  const [edit, setEdit] = useState(false);
 
   const createSession = async () => {
     axios
@@ -63,7 +64,9 @@ const UserInfo = () => {
     }
   };
 
-  return (
+  const handleSubmit = () => {};
+
+  return !edit ? (
     <div className="container bootstrap snippets bootdey">
       <div className="panel-body inf-content">
         <div className="row">
@@ -77,7 +80,9 @@ const UserInfo = () => {
               data-original-title="Usuario"
             />
             {/* TODO: Allow user to edit information */}
-            <button className="edit btn btn-link">Edit Information</button>
+            <button className="edit btn btn-link" onClick={() => setEdit(true)}>
+              Edit Information
+            </button>
           </div>
           <div className="col-md-6">
             <h2>Information</h2>
@@ -183,7 +188,216 @@ const UserInfo = () => {
         </div>
       </div>
     </div>
+  ) : (
+    <div className="container bootstrap snippets bootdey ">
+      <div className="panel-body inf-content ">
+        <div className="row">
+          <div className="col-md-4 img">
+            <img
+              alt=""
+              style={{ width: 500, borderRadius: "50%" }}
+              title=""
+              className="img-circle img-thumbnail isTooltip"
+              src="https://bootdey.com/img/Content/avatar/avatar7.png"
+              data-original-title="Usuario"
+            />
+            {/* <!-- Profile picture help block--> */}
+            <div className="small font-italic text-muted mb-3 image">
+              JPG or PNG no larger than 5 MB
+            </div>
+
+            <button className="btn btn-primary image-button" type="button">
+              Upload Image
+            </button>
+          </div>
+          <div className="col-md-6">
+            <h2>Information</h2>
+            <div className="table-responsive info-table">
+              <table className="table table-user-information">
+                <tbody>
+                  <tr className="edit-info">
+                    <td>
+                      <strong>
+                        <span className="glyphicon glyphicon-asterisk text-primary"></span>
+                        Identificacion
+                      </strong>
+                    </td>
+                    <td className="text-primary">
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={userData._id}
+                        disabled={true}
+                      />
+                    </td>
+                  </tr>
+                  <tr className="edit-info">
+                    <td>
+                      <strong>
+                        <span className="glyphicon glyphicon-user  text-primary"></span>
+                        First Name
+                      </strong>
+                    </td>
+                    <td className="text-primary">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder={userData.firstName}
+                      />
+                    </td>
+                  </tr>
+                  <tr className="edit-info">
+                    <td>
+                      <strong>
+                        <span className="glyphicon glyphicon-cloud text-primary"></span>
+                        Lastname
+                      </strong>
+                    </td>
+                    <td className="text-primary">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder={userData.lastName}
+                      />
+                    </td>
+                  </tr>
+                  <tr className="edit-info">
+                    <td>
+                      <strong>
+                        <span className="glyphicon glyphicon-eye-open text-primary"></span>
+                        Role
+                      </strong>
+                    </td>
+                    <td className="text-primary">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder={userData.userType}
+                        disabled={true}
+                      />
+                    </td>
+                  </tr>
+                  <tr className="edit-info">
+                    <td>
+                      <strong>
+                        <span className="glyphicon glyphicon-envelope text-primary"></span>
+                        Email
+                      </strong>
+                    </td>
+                    <td className="text-primary">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder={userData.email}
+                        disabled={true}
+                      />
+                    </td>
+                  </tr>
+                  {regularUser ? (
+                    <tr className="edit-info">
+                      <td>
+                        <strong>
+                          <span className="glyphicon glyphicon-envelope text-primary"></span>
+                          Subscribe Status
+                        </strong>
+                      </td>
+                      <td className="text-primary">
+                        <button
+                          className="btn btn-link"
+                          onClick={() => createSession()}
+                        >
+                          Click here to Subscribe
+                        </button>
+                      </td>
+                    </tr>
+                  ) : null}
+                  {subscribedUser ? (
+                    <tr className="edit-info">
+                      <td>
+                        <strong>
+                          <span className="glyphicon glyphicon-envelope text-primary"></span>
+                          Subscribe Status
+                        </strong>
+                      </td>
+                      <td className="text-primary">
+                        <button
+                          className="btn btn-link"
+                          onClick={() => cancelSubscription()}
+                        >
+                          Click here to Unsubscribe
+                        </button>
+                      </td>
+                    </tr>
+                  ) : null}
+                  {admin ? (
+                    <tr className="edit-info">
+                      <td>
+                        <strong>
+                          <span className="glyphicon glyphicon-envelope text-primary"></span>
+                          Subscribe Status
+                        </strong>
+                      </td>
+                      <td className="text-primary">Owner</td>
+                    </tr>
+                  ) : null}
+                  <tr className="edit-info">
+                    <td>
+                      <strong>
+                        <span className="glyphicon glyphicon-envelope text-primary"></span>
+                        Old Password
+                      </strong>
+                    </td>
+                    <td className="text-primary">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="•••••••••••••••••••"
+                      />
+                    </td>
+                  </tr>
+                  <tr className="edit-info">
+                    <td>
+                      <strong>
+                        <span className="glyphicon glyphicon-envelope text-primary"></span>
+                        New Password
+                      </strong>
+                    </td>
+                    <td className="text-primary">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="•••••••••••••••••••"
+                      />
+                    </td>
+                  </tr>
+                  <tr className="edit-info">
+                    <td>
+                      <strong>
+                        <span className="glyphicon glyphicon-envelope text-primary"></span>
+                        Confirm Password
+                      </strong>
+                    </td>
+                    <td className="text-primary">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="•••••••••••••••••••"
+                      />
+                    </td>
+                  </tr>
+                  <button className="btn btn-primary submit" type="submit">
+                    Update Profile
+                  </button>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 export default UserInfo;
 //use this to fix the style: https://www.bootdey.com/snippets/view/Table-user-information
+
+//folow this for update user info: https://stackoverflow.com/questions/65062187/need-to-have-the-user-be-able-to-edit-profile-with-an-edit-button
